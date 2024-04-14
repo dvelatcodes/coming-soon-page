fe6ef8b8020d51e20e2843d343e0fe589d637eb0
love olubummo <olubummolove3@gmail.com>
2024-04-14 13:08:53 +0100

    feature: introduced the error icon and error message

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 
 const btn = document.querySelector('.btn-clicker');
 const formField = document.querySelector('input');
+const message = document.querySelector('small');
+const errorIcon = document.querySelector('.error-icon');
 
 btn.addEventListener('click', () => {
-    if (formField.value == "") alert("Wrong validation, please fill form appropriately");
-    else if (formField.value.match(/[!-\-]|[:-\?]|[\[-`]|[{-~}]]/)) alert("Special characters not allowed");
+    if (formField.value == "") {
+        errorIcon.style.display = 'block';
+        message.style.color = 'hsl(0, 93%, 68%)';
+        message.innerHTML = 'Form cannot be left empty';
+        message.style.display = 'block';
+    }
+    else if (formField.value.match(/[!-\-]|[:-\?]|[\[-`]|[{-~}]]/)) {
+        errorIcon.style.display = 'block';
+        message.style.color = 'hsl(0, 93%, 68%)';
+        message.innerHTML = 'Special characters not allowed';
+        message.style.display = 'block';
+    }
     else if (!formField.value.match(/[a-zA-Z][0-9]@gmail.com/) && !formField.value.match(/[a-zA-Z]@gmail.com/)) {
-        alert("Wrong validation, please fill form appropriately");
+        errorIcon.style.display = 'block';
+        message.style.color = 'hsl(0, 93%, 68%)';
+        message.innerHTML = 'Please provide a valid email';
+        message.style.display = 'block';
     }
     else if (formField.value.match(/[a-zA-Z][0-9]@gmail.com/)) {
-        alert("successfully filled")
+        errorIcon.style.display = 'none';
+        message.style.color = 'green';
+        message.innerHTML = "successfully filled";
+        message.style.display = 'block';
         formField.value = ""
     }
     else if (formField.value.match(/[a-zA-Z]@gmail.com/)) {
-        alert("successfully filled")
+        errorIcon.style.display = 'none';
+        message.style.color = 'green';
+        message.innerHTML = "successfully filled";
+        message.style.display = 'block';
         formField.value = ""
     }
 })
